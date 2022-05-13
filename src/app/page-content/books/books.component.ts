a94aafc005c2233878d875e675c9a512b3a104da
Mateusz Hadrian <mhadrian@pgs-soft.com>
2022-05-13 17:40:27 +0200

    created PreventAccidentalLeavingGuard (prevent-accidental-leaving.guard.ts) and connected it to route 'base/books' with canDeactivate property

--- src/app/page-content/books/books.component.ts
+++ src/app/page-content/books/books.component.ts
@@ -1,18 +1,24 @@
 import {Component} from '@angular/core';
 import {PageContentBaseService} from '../page-content-base.service';
 import {BookDto} from '../../../utils/book.dto';
+import {CanComponentDeactivate} from './prevent-accidental-leaving.guard';
 import {Observable} from 'rxjs';
+import {UrlTree} from '@angular/router';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
+export class BooksComponent  implements CanComponentDeactivate {
   books$: Observable<BookDto[]> = this.pageContentBaseService.getBooks$();
 
   constructor(
     private pageContentBaseService: PageContentBaseService,
   ) { }
+
+  // canDeactivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //
+  // }
 
 }
