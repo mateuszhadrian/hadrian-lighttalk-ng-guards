53d5fc62b3cbe1d4528e4a75927ca2ff8dcd2642
Mateusz Hadrian <mhadrian@pgs-soft.com>
2022-05-19 15:38:05 +0200

    canDeactivate adjusted to new changes

--- src/app/page-content/books/books.component.ts
+++ src/app/page-content/books/books.component.ts
@@ -1,24 +1,39 @@
 import {Component} from '@angular/core';
 import {PageContentBaseService} from '../page-content-base.service';
 import {BookDto} from '../../../utils/book.dto';
-import {CanComponentDeactivate} from './prevent-accidental-leaving.guard';
 import {Observable} from 'rxjs';
 import {UrlTree} from '@angular/router';
+import {CanComponentDeactivate} from './prevent-accidental-leaving.guard';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent  implements CanComponentDeactivate {
+export class BooksComponent implements CanComponentDeactivate {
   books$: Observable<BookDto[]> = this.pageContentBaseService.getBooks$();
+  isEditMode: boolean = false;
+  editModeCounter: number = 0;
+  areEditedItems: boolean = false;
 
   constructor(
     private pageContentBaseService: PageContentBaseService,
   ) { }
 
-  // canDeactivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //
-  // }
+  setEditModeCounter(isEditMode: boolean){
+    if (isEditMode){
+      this.editModeCounter++
+    } else {
+      this.editModeCounter--
+    }
+  }
+
+  canDeactivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.editModeCounter && !this.isEditMode) {
+      return confirm('Masz włączony tryb edycji. Czy na pewno chcesz opuścić kategorię?')
+    } else {
+      return true
+    }
+  }
 
 }
